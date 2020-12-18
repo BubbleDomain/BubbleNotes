577ef03710c434f86e73db53c46126ba983acec5
yaobeihaoyu <yaobeihaoyu@bilibili.com>
2020-12-18 11:01:54 +0800

    [FEATURE] 589.n-ary-tree-preorder-traversal

--- algorithms/C++/datastructure/tree/589.cpp
+++ algorithms/C++/datastructure/tree/589.cpp
@@ -29,7 +29,17 @@ public:
 
 
 vector<int> preorder(Node* root) {
-
+    vector<int> r;
+    if (root == NULL) return r;
+    stack<Node*> s;
+    s.push(root);
+    while(!s.empty()) {
+        auto top = s.top();
+        s.pop();
+        r.push_back(top->val);
+        for (int i = top->children.size() - 1; i >= 0; i--) s.push(top->children[i]);
+    }
+    return r;
 }
 
 int main() {
