105cce389d349f23d3d4ab55fcfb3532007ec464
yaobeihaoyu <yaobeihaoyu@bilibili.com>
2020-12-22 21:41:41 +0800

    [FEATURE] 46

---
+++ algorithms/C++/algorithm/recursion/46.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @author: batteria
+ * @version: 1.0
+ * @since: 2020/12/22
+ * @description: 46.permutations
+ */
+vector<vector<int>> ans;
+
+void recurse(vector<int>& nums, int i, int length) {
+
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+    recurse(nums, -1, nums.size());
+    return ans;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
