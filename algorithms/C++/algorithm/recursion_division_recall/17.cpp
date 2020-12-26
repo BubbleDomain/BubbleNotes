862b653d53033dc3ea8dd4884c1c33fce45762ba
yaobeihaoyu <yaobeihaoyu@bilibili.com>
2020-12-26 22:26:56 +0800

    [FEATURE] 17

---
+++ algorithms/C++/algorithm/recursion_division_recall/17.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+
+/**
+ * @author: batteria
+ * @version: 1.0
+ * @since: 2020/12/26
+ * @description: 17.letter-combinations-of-a-phone-number
+ */
+vector<vector<char>> a{{'a', 'b', 'c'},
+                       {'d', 'e', 'f'},
+                       {'g', 'h', 'i'},
+                       {'j', 'k', 'l'},
+                       {'m', 'n', 'o'},
+                       {'p', 'q', 'r', 's'},
+                       {'t', 'u', 'v'},
+                       {'w', 'x', 'y', 'z'}};
+
+vector<string> letterCombinations(string digits) {
+    vector<string> ans;
+    for (auto i : digits) {
+        
+    }
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
