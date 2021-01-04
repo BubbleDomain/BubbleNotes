c3ad2b6915ce6c3eff5eb5bee08e46bb5b1e9346
yaobeihaoyu <yaobeihaoyu@bilibili.com>
2021-01-04 16:01:28 +0800

    17.letter-combinations-of-a-phone-number

--- algorithms/C++/algorithm/recursion_division_recall/17.cpp
+++ algorithms/C++/algorithm/recursion_division_recall/17.cpp
@@ -20,15 +20,32 @@ vector<vector<char>> a{{'a', 'b', 'c'},
                        {'p', 'q', 'r', 's'},
                        {'t', 'u', 'v'},
                        {'w', 'x', 'y', 'z'}};
+vector<string> ans;
 
-vector<string> letterCombinations(string digits) {
-    vector<string> ans;
-    for (auto i : digits) {
-        
+void recurse(string digits, string &temp, int level, int limit) {
+    if (level == limit) {
+        ans.push_back(temp);
+        return;
+    }
+    auto v = a[digits[level] - '2'];
+    for (auto i : v) {
+        temp += i;
+        recurse(digits, temp, level + 1, limit);
+        temp.erase(temp.end() - 1);
     }
 }
 
+vector<string> letterCombinations(string digits) {
+    int size = digits.size();
+    if (size == 0)
+        return ans;
+    string t = "";
+    recurse(digits, t, 0, size);
+    return ans;
+}
+
 int main() {
+    auto a = letterCombinations("23");
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
