e19c5dd6fa5cba41ada7485c0d69c7fc52728b6e
yaobeihaoyu <yaobeihaoyu@bilibili.com>
2021-04-06 21:53:33 +0800

    36

--- algorithms/Java/src/algorithm/search/36.java
+++ algorithms/Java/src/algorithm/search/36.java
@@ -2,7 +2,7 @@ package algorithm.search;
 
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        if (!solve(board, 1) || !solve(board, 2)) return false
+        if (!solve(board, 1) || !solve(board, 2)) return false;
         int[] dx1 = {0, 3, 6, 0, 3, 6, 0, 3, 6};
         int[] dy1 = {0, 0, 0, 3, 3, 3, 6, 6, 6};
         int[] dx2 = {0, 1, 2, 0, 1, 2, 0, 1, 2};
@@ -12,22 +12,27 @@ class Solution {
             int y = dy1[i];
             int state = 0;
             for (int j = 0; j < 9; j++) {
-                char c = board[j][i];
+                char c = board[x + dx2[j]][y + dy2[j]];
+                if (c == '.') continue;
+                int t = c - '1';
+                if ((state & 1 << t) > 0) return false;
+                state += 1 << t;
             }
         }
         return true;
     }
 
-    boolean solve(char[][] board, int type) {a
+    boolean solve(char[][] board, int type) {
         for (int i = 0; i < 9; i++) {
             int state = 0;
             for (int j = 0; j < 9; j++) {
                 char c = type == 1 ? board[i][j] : board[j][i];
                 if (c == '.') continue;
                 int t = c - '1';
-                if (state & 1 >> t > 0) return false;
-                state += 1 >> t;
+                if ((state & 1 << t) > 0) return false;
+                state += 1 << t;
             }
         }
+        return true;
     }
 }
